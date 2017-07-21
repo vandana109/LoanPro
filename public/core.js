2088db0e52a35f59cd4e230025b1ac92f96584d2
vandana109 <vandana109@users.noreply.github.com>
2017-07-21 12:32:15 +0530

    first commit- included form for adding user and their listings
    run npm install to install the dependencies

---
+++ public/core.js
@@ -0,0 +1,60 @@
+// public/core.js
+angular.module('scotchTodo', [])
+.controller("mainController", function($scope, $http){
+
+    $scope.formData = {};
+
+    // when landing on the page, get all todos and show them
+    $http.get('/api/todos')
+        .success(function(data) {
+            $scope.todos = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+    // when submitting the add form, send the text to the node API
+    $scope.createTodo = function() {
+        console.log( $scope.formData);
+        $scope.formData.maturityAmount= 2*$scope.formData.depositAmount;
+        var maturityDate = new Date($scope.formData.depositDate);
+        maturityDate.setFullYear(maturityDate.getFullYear() + 1);
+        $scope.formData.maturityDate= new Date(maturityDate.setDate(maturityDate.getDate() - 1));
+        $http.post('/api/todos', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {}; // clear the form so our user is ready to enter another
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    // delete a todo after checking it
+    $scope.deleteTodo = function(id) {
+        $http.delete('/api/todos/' + id)
+            .success(function(data) {
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+})
+.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: '/index.html',
+        controller: 'mainController'
+      })
+      .when('/takeLoan', {
+        templateUrl: '/takeLoan.html',
+        controller: 'takeLoanController'
+      });
+  }]);
+
+
